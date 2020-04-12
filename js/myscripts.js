fd7f1bae9764ed26ac590203f53573b4957d3d9d
magvtv <magutujr@gmail.com>
2020-04-12 23:07:06 +0300

    feeling tired did some js and modified my html

--- js/myscripts.js
+++ js/myscripts.js
@@ -1,1 +1,18 @@
 alert("Hello Bitch!");
+
+$(".clickA").click(function(){
+     $(".des-hidden").slideToggle();
+     $(".des-showing").slideToggle();
+});
+
+$(".clickB").click(function(){
+    $(".dev-hidden").slideToggle();
+    $(".dev-showing").slideToggle();
+});
+
+$(".clickC").click(function(){
+     $(".manag-hidden").slideToggle();
+     $(".manag-showing").slideToggle(); 
+});
+
+
